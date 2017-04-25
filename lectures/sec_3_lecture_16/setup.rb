e1a72007ff6bc2a2058e61dc118987cbfd7e379a
Adam Parkin <pzelnip@gmail.com>
2017-04-25 21:52:02 +0000

    Add example of installing packages

---
+++ lectures/sec_3_lecture_16/setup.rb
@@ -0,0 +1,14 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+file '/etc/motd' do
+  content "This server is the property of Adam Parkin\n"
+  group 'root'
+  owner 'root'
+  action :create
+end
