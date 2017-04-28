2faa15047b788aa66a708a8ccf75f05735424cbf
Adam Parkin <pzelnip@gmail.com>
2017-04-28 17:42:10 +0000

    Add mot template to workstation cookbook

---
+++ lectures/sec_7_lecture_34/cookbooks/workstation/recipes/setup.rb
@@ -0,0 +1,16 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+template '/etc/motd' do
+  source 'motd.erb'
+  action :create
+end
+
+package 'git' do
+  action :install
+end
